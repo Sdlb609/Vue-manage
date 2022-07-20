63010fd7eadf2497fd3a68c54804fe12d8cddeb4
sdlb <sdlb@666.com>
2022-07-21 07:48:15 +0800

    token

---
+++ vue-manage/store/user.js
@@ -0,0 +1,22 @@
+//缓存后端返回的token
+import Cookie from "js-cookie";
+export default{
+    state:{
+        token: ''
+    },
+    mutations:{
+        setToken(state, val){
+            state.token = val
+            Cookie.set('token', val)
+        },
+        //清除token
+        clearToken(state){
+            state.token = ''
+            Cookie.remove('token')
+        },
+        //获取token
+        getToken(state){
+            state.token = state.token || Cookie.get('token')
+        }
+    }
+}
